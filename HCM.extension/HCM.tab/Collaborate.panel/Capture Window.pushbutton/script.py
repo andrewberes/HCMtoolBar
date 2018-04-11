907aecde4d9e74a1d8f09ffc8ca32a664ae0a065
Andrew Beres <andrew.steven.beres@gmail.com>
2018-04-11 15:11:42 -0400

    Capture Window adjustment
    to create .JPG file rather than .PNG file

--- HCM.extension/HCM.tab/Collaborate.panel/Capture Window.pushbutton/script.py
+++ HCM.extension/HCM.tab/Collaborate.panel/Capture Window.pushbutton/script.py
@@ -24,17 +24,17 @@ f.close()
 
 # collect file location from user
 dialog = Forms.SaveFileDialog()
-dialog.Title = 'Export current view as PNG'
-dialog.Filter = 'PNG files (*.PNG)|*.PNG'
+dialog.Title = 'Export current view as JPG'
+dialog.Filter = 'JPG files (*.JPG)|*.JPG'
 
 if dialog.ShowDialog() == Forms.DialogResult.OK:
     # set up the export options
     options = DB.ImageExportOptions()
     options.ExportRange = DB.ExportRange.VisibleRegionOfCurrentView
     options.FilePath = dialog.FileName
-    options.HLRandWFViewsFileType = DB.ImageFileType.PNG
+    #options.HLRandWFViewsFileType = DB.ImageFileType.PNG
     options.ImageResolution = DB.ImageResolution.DPI_600
     options.ZoomType = DB.ZoomFitType.Zoom
-    options.ShadowViewsFileType = DB.ImageFileType.PNG
+    #options.ShadowViewsFileType = DB.ImageFileType.PNG
 
     revit.doc.ExportImage(options)
