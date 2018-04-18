2c1a49159131731362e65aef6741dca97c91d034
Andrew Beres <andrew.steven.beres@gmail.com>
2018-04-18 15:11:43 -0400

    Add 'Wipe for Consultant' button
    for stripping a model of sheets, views and additional information not necessary to share with outside consultants or clients.

---
+++ HCM.extension/HCM.tab/Collaborate.panel/Wipe Model Components.pushbutton/script.py
@@ -0,0 +1,96 @@
+import inspect
+import pyrevittoolslib.wipeactions
+
+from pyrevit import forms
+from pyrevit import script
+import pyrevit
+import os
+import datetime
+
+__title__ = 'Wipe for\nConsultant'
+
+
+__doc__ = 'This tools helps you to remove extra unnecessary information in '\
+          'the model when sending to a contractor or consultant. '\
+          'Run the tools and select the categories that you\'d like '\
+          'to be removed from the model. Then hit \"Wipe Model\" and '\
+          'the process will go through each category and will ' \
+          'remove them. You might see some errors or warnings from Revit '\
+          '(since this is a very distructive) ' \
+          'process but generally they should not crash the script.'
+
+
+logger = script.get_logger()
+
+#buttom Tracker
+now = datetime.datetime.now()
+date = now.strftime("%Y-%m-%d")
+hostapp = pyrevit._HostApplication(__revit__)
+userName = hostapp.username
+buttonCode = 'Wipe for Consultant'
+logFilePath = 'L:\Dynamo\zzz.DoNotModify\pyHCMuserLogs'
+os.chdir(logFilePath)
+f = open(userName + ".txt", "a+")
+f.write(buttonCode + "\t" + date +"\n")
+f.close()
+
+
+class WipeOption:
+    def __init__(self, name, default_state=False, wipe_action=None):
+        self.name = name
+        self.state = default_state
+        self.wipe_action = wipe_action
+        self.is_dependent = self.wipe_action.is_dependent
+
+    def __repr__(self):
+        return '<WipeOption Name:{} State:{} Action:{}>'\
+               .format(self.name, self.state, self.wipe_action)
+
+    def __bool__(self):
+        return self.state
+
+    def __nonzero__(self):
+        return self.state
+
+
+# generate wipe options based on functions in
+# pyrevittoolslib.wipeactions module
+wipe_options = []
+
+for mem in inspect.getmembers(pyrevittoolslib.wipeactions):
+    moduleobject = mem[1]
+    if inspect.isfunction(moduleobject):
+        if moduleobject.__doc__:
+            wipe_options.append(WipeOption(moduleobject.__doc__,
+                                           wipe_action=moduleobject))
+
+# generate wipe options based on model worksets
+for wscleaner_func in pyrevittoolslib.wipeactions.get_worksetcleaners():
+    if wscleaner_func.__doc__:
+        wipe_options.append(WipeOption(wscleaner_func.__doc__,
+                                       wipe_action=wscleaner_func))
+
+
+# ask user for wipe actions
+return_options = \
+    forms.SelectFromCheckBoxes.show(
+        sorted(wipe_options, key=lambda x: x.name),
+        title='Wipe Options',
+        width=500,
+        button_name='Wipe Model'
+        )
+
+if return_options:
+    dependent_actions = [wipe_act
+                         for wipe_act in return_options
+                         if wipe_act.is_dependent]
+
+    not_dependent_actions = [wipe_act
+                             for wipe_act in return_options
+                             if not wipe_act.is_dependent]
+
+    for actions in [dependent_actions, not_dependent_actions]:
+        for wipe_act in actions:
+            if wipe_act:
+                logger.debug('Calling: {}'.format(wipe_act))
+                wipe_act.wipe_action()
